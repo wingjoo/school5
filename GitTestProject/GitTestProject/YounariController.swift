b57b044c983c75d8fa2af497bd874d1afd07c2ff
younari <instar08@gmail.com>
2017-11-24 11:08:51 +0900

    zico branch

--- GitTestProject/GitTestProject/YounariController.swift
+++ GitTestProject/GitTestProject/YounariController.swift
@@ -14,6 +14,7 @@ class YounariController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorView?.backgroundColor = #colorLiteral(red: 1, green: 0.6632423401, blue: 0, alpha: 1)
     }
     
     override func didReceiveMemoryWarning() {
