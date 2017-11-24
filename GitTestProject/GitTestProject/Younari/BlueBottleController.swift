75499976047288ea41a432d878d93a7ab31ea0b1
younari <instar08@gmail.com>
2017-11-24 12:34:29 +0900

    zico

--- GitTestProject/GitTestProject/Younari/BlueBottleController.swift
+++ GitTestProject/GitTestProject/Younari/BlueBottleController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class BlueBottleController: UIViewController {
-
     
     @IBOutlet weak var bluebottle: UIView!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         bluebottle.backgroundColor = .blue
@@ -22,16 +22,6 @@ class BlueBottleController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
