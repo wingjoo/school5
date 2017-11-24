d261b687cf75961d66da07096190538228eafade
younari <instar08@gmail.com>
2017-11-24 11:00:38 +0900

    younari first push

---
+++ GitTestProject/GitTestProject/YounariController.swift
@@ -0,0 +1,23 @@
+//
+//  YounariController.swift
+//  GitTestProject
+//
+//  Created by 김기윤 on 24/11/2017.
+//  Copyright © 2017 youngmin joo. All rights reserved.
+//
+
+import UIKit
+
+class YounariController: UIViewController {
+
+    @IBOutlet weak var colorView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
