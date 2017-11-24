3cb78c21a4b50feb098ebb4dbb8742305813fc94
younari <instar08@gmail.com>
2017-11-24 11:56:17 +0900

    dd

---
+++ GitTestProject/GitTestProject/Younari/BlueBottleController.swift
@@ -0,0 +1,35 @@
+//
+//  BlueBottleController.swift
+//  GitTestProject
+//
+//  Created by 김기윤 on 24/11/2017.
+//  Copyright © 2017 youngmin joo. All rights reserved.
+//
+
+import UIKit
+
+class BlueBottleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
