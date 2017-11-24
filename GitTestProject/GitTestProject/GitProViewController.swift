05b1fc845491d2c60fd2172a2d5cb05a4375c5f5
spicystar <skh0206@gmail.com>
2017-11-24 11:53:49 +0900

    commit stash

---
+++ GitTestProject/GitTestProject/GitProViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GitProViewController.swift
+//  GitTestProject
+//
+//  Created by Kyuhan Shin on 2017. 11. 24..
+//  Copyright © 2017년 youngmin joo. All rights reserved.
+//
+
+import UIKit
+
+class GitProViewController: UIViewController {
+
+    
+    @IBOutlet weak var simpleLB: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func testBtn(_ sender: Any) {
+    }
+    
+}
